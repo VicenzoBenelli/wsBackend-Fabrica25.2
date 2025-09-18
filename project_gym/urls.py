e00a9ca9db209bd5afc66e27ac6ca0df8bac2f14
VicenzoBenelli <vicenzob.silva@gmail.com>
2025-09-18 18:51:11 -0300

    Feat: Projeto finalizado

--- project_gym/urls.py
+++ project_gym/urls.py
@@ -7,8 +7,9 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('', include('app.urls')),  
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
+    path('app/', include('app.urls')),  
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),    
